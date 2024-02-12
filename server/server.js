548a95f47522d68df38a4dff1ef6832c7f5509a7
Mahagola <suryanshyadav885@gmail.com>
2024-02-12 07:11:34 +0530

    added files

---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: "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",
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello World",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      temperature: 0,
+    });
+    console.log(response.choices[0].message);
+    return res.status(200).send({
+      bot: response.choices[0].message.content,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
+  }
+});
+
+app.listen(5000, () =>
+  console.log("AI server started on http://localhost:5000")
+);
